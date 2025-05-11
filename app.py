d325f35cfabfe7c0cd9afc785bcc2df79067ab7e
BakiBakiSudo61 <yu.aoyama0611@gmail.com>
2025-05-12 01:10:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ st.set_page_config(page_title="YouTube Downloader", layout="centered") # ペー�
 st.markdown("""
 <style>
 div.stButton > button {
+    display: block;
     margin: 0 auto;
 }
 </style>
