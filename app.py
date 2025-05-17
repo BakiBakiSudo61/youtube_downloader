ba77630667847c90b3ea4062f3e39b9e404d13e9
BakiBakiSudo61 <yu.aoyama0611@gmail.com>
2025-05-17 17:36:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -50,9 +50,8 @@ st.markdown(
     }}
 
     </style>
-    """,
+    """)
     st.markdown(unsafe_allow_html=True)
-)
 
 # まずログインボタンを表示し、ユーザーにログインを促す
 if not st.user.is_logged_in: # ここでエラーが発生している可能性
